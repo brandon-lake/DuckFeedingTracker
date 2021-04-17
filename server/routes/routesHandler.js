b0da40cff735ff23b748acfa2a34e8459e5c17fb
Brandon Lake <blakelake12@gmail.com>
2021-04-17 15:12:56 -0700

    Nav styling, install axios

--- server/routes/routesHandler.js
+++ server/routes/routesHandler.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let Feeding = require('../models/feeding.model');
+const Feeding = require('../models/feeding.model');
 
 router.route("/view").get((req, res) => {
     Feeding.find()
@@ -10,12 +10,24 @@ router.route("/view").get((req, res) => {
         });
 });
 
-router.route("/add").get((req, res) => {
-    res.json({"msg": "Add a Feeding (from backend)"});
-});
-
 router.route("/add").post((req, res) => {
     // handle form submits to update db
+    let numberOfDucks = req.body.numberOfDucks;
+    let feedingLocation = req.body.feedingLocation;
+    let feedingTime = req.body.feedingTime;
+    let food = req.body.food;
+    let foodQuantity = req.body.foodQuantity;
+
+    let newFeeding = new Feeding({
+        numberOfDucks, feedingLocation, feedingTime, food, foodQuantity
+    });
+
+    newFeeding.save()
+        .then(() => res.json("Feeding submitted!"))
+        .catch(err => {
+            // can log error message here if logging is setup
+            res.status(400).json("Error - Could not add new feeding record.");
+        });
 });
 
 module.exports = router;
